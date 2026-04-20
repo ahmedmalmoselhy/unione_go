5ad11dee500a92bbd663e82e97ce9f460841d23a
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2026-04-20 09:06:14 +0200

    feat: add update and delete methods to OrgRepository

--- internal/repository/org_repository.go
+++ internal/repository/org_repository.go
@@ -8,13 +8,21 @@ import (
 type OrgRepository interface {
 	CreateUniversity(u *models.University) error
 	GetUniversities() ([]models.University, error)
+	GetUniversityByID(id uint) (*models.University, error)
+	UpdateUniversity(u *models.University) error
+	DeleteUniversity(id uint) error
 	
 	CreateFaculty(f *models.Faculty) error
 	GetFacultiesByUniversity(uniID uint) ([]models.Faculty, error)
 	GetFacultyByID(id uint) (*models.Faculty, error)
+	UpdateFaculty(f *models.Faculty) error
+	DeleteFaculty(id uint) error
 	
 	CreateDepartment(d *models.Department) error
 	GetDepartmentsByFaculty(facultyID uint) ([]models.Department, error)
+	GetDepartmentByID(id uint) (*models.Department, error)
+	UpdateDepartment(d *models.Department) error
+	DeleteDepartment(id uint) error
 }
 
 type orgRepository struct {
@@ -37,6 +45,22 @@ func (r *orgRepository) GetUniversities() ([]models.University, error) {
 	return unis, nil
 }
 
+func (r *orgRepository) GetUniversityByID(id uint) (*models.University, error) {
+	var uni models.University
+	if err := r.db.First(&uni, id).Error; err != nil {
+		return nil, err
+	}
+	return &uni, nil
+}
+
+func (r *orgRepository) UpdateUniversity(u *models.University) error {
+	return r.db.Save(u).Error
+}
+
+func (r *orgRepository) DeleteUniversity(id uint) error {
+	return r.db.Delete(&models.University{}, id).Error
+}
+
 func (r *orgRepository) CreateFaculty(f *models.Faculty) error {
 	return r.db.Create(f).Error
 }
@@ -57,6 +81,14 @@ func (r *orgRepository) GetFacultyByID(id uint) (*models.Faculty, error) {
 	return &faculty, nil
 }
 
+func (r *orgRepository) UpdateFaculty(f *models.Faculty) error {
+	return r.db.Save(f).Error
+}
+
+func (r *orgRepository) DeleteFaculty(id uint) error {
+	return r.db.Delete(&models.Faculty{}, id).Error
+}
+
 func (r *orgRepository) CreateDepartment(d *models.Department) error {
 	return r.db.Create(d).Error
 }
@@ -67,4 +99,20 @@ func (r *orgRepository) GetDepartmentsByFaculty(facultyID uint) ([]models.Depart
 		return nil, err
 	}
 	return depts, nil
+}
+
+func (r *orgRepository) GetDepartmentByID(id uint) (*models.Department, error) {
+	var dept models.Department
+	if err := r.db.First(&dept, id).Error; err != nil {
+		return nil, err
+	}
+	return &dept, nil
+}
+
+func (r *orgRepository) UpdateDepartment(d *models.Department) error {
+	return r.db.Save(d).Error
+}
+
+func (r *orgRepository) DeleteDepartment(id uint) error {
+	return r.db.Delete(&models.Department{}, id).Error
 }
